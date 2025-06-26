d8614f017651f004a8f3f30221ab035d74422ada
Stefania Bibart <stefania.bibart@emeal.nttdata.com>
2025-06-26 14:47:29 +0300

    run ng add @angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,6 +18,11 @@ import { DataServiceFactory } from './shared/services/data-service-factory';
 import { ParkingSpotService } from './shared/services/parking-spot.service';
 import { LocalStorageParkingSpotService } from './shared/services/localstorage-parking-spot.service';
 import { FirebaseParkingSpotService } from './shared/services/firebase-parking-spot.service';
+import {
+  initializeApp as initializeApp_alias,
+  provideFirebaseApp,
+} from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 const firebaseConfig = {
   apiKey: environment.FIREBASE_AUTH_KEY,
@@ -78,5 +83,19 @@ export const appConfig: ApplicationConfig = {
       },
       deps: [DataServiceFactory],
     },
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'parking-app-16597',
+        appId: '1:1050487202550:web:8bd92aff6274dc029902d7',
+        databaseURL:
+          'https://parking-app-16597-default-rtdb.europe-west1.firebasedatabase.app',
+        storageBucket: 'parking-app-16597.firebasestorage.app',
+        apiKey: 'AIzaSyB9peE6Hg6j3mUM72h8y9dowfQm0l4LdLc',
+        authDomain: 'parking-app-16597.firebaseapp.com',
+        messagingSenderId: '1050487202550',
+        measurementId: 'G-17KJXQ5T1B',
+      })
+    ),
+    provideAuth(() => getAuth()),
   ],
 };
